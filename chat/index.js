bd0b9d3833e95ab7efd40f631d256a25b8b7d2e3
김현준 <supply900219@gmail.com>
2023-03-17 14:45:59 +0900

    Feat:socket.io 구현

--- chat/index.js
+++ chat/index.js
@@ -1,13 +1,40 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const mongoose = require("mongoose");
+const io = socket(server);
+const cors = require("cors")
 
+const port = process.env.CHAT_PORT || 5000;
+
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
+io.on("connection", (socket) => {
+  console.log(`${nickName}님이 접속하였습니다.`);
+
+  socket.on("disconnect", () => {
+    console.log(`${nickName}님이 나갔습니다.`);
+  });
+
+  socket.on("new message", async (data) => {
+    const message = new Message({
+      name: data.name,
+      message: data.message,
+    });
+    await message.save();
+  });
+
+  socket.on("typing", (data) => {
+    
+  })
+});
+
+// 몽고DB 연결
 async function start() {
   try {
     await mongoose.connect(process.env.MONGODB_CONNECTION);
