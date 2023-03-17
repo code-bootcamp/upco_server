56b2cdb0f91f944fbe60d821ba13c1c63f8bbecd
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-03-17 15:05:12 +0900

    Feat: auth module 추가

---
+++ nestjs/src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([]),
+  ],
+  providers: [
+  ],
+})
+export class AuthModule {}
