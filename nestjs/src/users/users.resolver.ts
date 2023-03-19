271f1fe6c1fc84c4473e8f729f0c64ffbede2cb8
turnaroundwoo <seowooo3117@gmail.com>
2023-03-19 20:17:21 +0900

    Feat: User API 추가
    #27 updateUserPwd
    #28 fetchLoginUser
    #30 updateUser
    #31 deleteUser

--- nestjs/src/users/users.resolver.ts
+++ nestjs/src/users/users.resolver.ts
@@ -2,6 +2,8 @@ import { Args, Mutation, Resolver, Query, Context } from "@nestjs/graphql";
 import { PublicAccess } from "src/common/decorator/public-access";
 import { IContext } from "src/common/interfaces/context";
 import { CreateUserInput } from "./dto/create-user.dto";
+import { UpdateAllInput } from "./dto/update-all.input";
+import { UpdateUserInput } from "./dto/update-user.input";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -19,6 +21,7 @@ export class UsersResolver {
     return this.usersService.create({ createUserInput });
   }
 
+  @PublicAccess()
   @Query(() => User)
   fetchId(
     @Args("email") email: string, //
@@ -34,6 +37,38 @@ export class UsersResolver {
     return this.usersService.findLogin({ userId });
   }
 
-  // 비밀번호 재설정
-  // 회원 정보 업데이트
+  @PublicAccess()
+  @Mutation(() => User)
+  updateUserPwd(
+    @Context() context: IContext,
+    @Args("updateUserInput") updateUserInput: UpdateUserInput,
+  ): Promise<User> {
+    const id = context.req.user.id;
+    console.log("✅", id);
+    return this.usersService.update({ id, updateUserInput });
+  }
+
+  // 회원 정보 업데이트 (email, pwd, age, interest, friends, image)
+  @Mutation(() => User)
+  updateUser(
+    @Context() context: IContext,
+    @Args("updateAllInput") updateAllInput: UpdateAllInput,
+  ): Promise<User> {
+    const id = context.req.user.id;
+    console.log("✅", id);
+    return this.usersService.updateAll({ id, updateAllInput });
+  }
+
+  @Mutation(() => Boolean)
+  deleteUser(
+    @Args("id") id: string, //
+  ): Promise<boolean> {
+    return this.usersService.delete({ id });
+  }
+
+  // 친구 추가
+
+  // 친구 목록 조회
+
+  // 친구 삭제
 }
