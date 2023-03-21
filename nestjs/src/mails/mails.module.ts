5fa133fa169fd3447058cedbf0d0064d8d5e9b07
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-03-21 17:01:41 +0900

    Feat : passwordResetMailer API 임시비밀번호 전달 로직 추가

--- nestjs/src/mails/mails.module.ts
+++ nestjs/src/mails/mails.module.ts
@@ -1,10 +1,16 @@
 import { MailerModule } from "@nest-modules/mailer";
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/users/entities/user.entity";
+import { UsersService } from "src/users/users.service";
 import { MailResolver } from "./mails.resolver";
 import { MailService } from "./mails.service";
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([
+      User, //
+    ]),
     MailerModule.forRootAsync({
       useFactory: () => ({
         transport: {
@@ -20,6 +26,7 @@ import { MailService } from "./mails.service";
   providers: [
     MailResolver, //
     MailService, //
+    UsersService,
   ],
 })
 export class MailModule {}
--- nestjs/src/mails/mails.service.ts
+++ nestjs/src/mails/mails.service.ts
@@ -1,25 +1,45 @@
 import { MailerService } from "@nest-modules/mailer";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotAcceptableException } from "@nestjs/common";
+import { IUsersServiceUpdateInput } from "src/users/interfaces/user-service.interface";
 import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class MailService {
   constructor(
     private readonly mailerService: MailerService, //
+    private readonly userService: UsersService, //
   ) {}
 
+  renderNewPassword() {
+    return String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  }
+
   async passwordResetMailer({ email }): Promise<string> {
+    const user = this.userService.findOneByEmail({ email });
+    if (!user) throw new NotAcceptableException();
+
+    // 임시 비밀번호 생성 로직 및 업데이트 로직입니다.
+    const renderNewPassword = this.renderNewPassword();
+    const input: IUsersServiceUpdateInput = {
+      id: (await user).id,
+      updateUserPwdInput: {
+        password: renderNewPassword,
+      },
+    };
+    await this.userService.update(input);
+
+    // 메일 전송 로직입니다.
     try {
       await this.mailerService.sendMail({
         to: email,
         from: process.env.MAIL_AUTH_USER,
-        subject: "test입니다.",
-        html: `비밀번호는 ㅇㅇㅇㅇ입니다.`,
+        subject: "[upco] : 임시비밀번호를 안내드립니다.",
+        html: `회원님의 임시 비밀번호는 ${renderNewPassword}입니다.`,
       });
-      return "전송 완료";
     } catch (error) {
       console.log(error);
-      return "전송 실패";
+      throw new NotAcceptableException();
     }
+    return "전송 완료";
   }
 }
