b6dd30d26c79d4b2e4217290c97b44a27c9e901e
turnaroundwoo <seowooo3117@gmail.com>
2023-03-28 11:01:52 +0900

    Feat: User API 수정 (interest 내용 추가)

--- nestjs/src/users/users.resolver.ts
+++ nestjs/src/users/users.resolver.ts
@@ -2,7 +2,8 @@ import { Args, Mutation, Resolver, Query, Context } from "@nestjs/graphql";
 import { PublicAccess } from "src/common/decorator/public-access";
 import { IContext } from "src/common/interfaces/context";
 import { CreateUserInput } from "./dto/create-user.dto";
-import { UpdateUserInput } from "./dto/update-user.input";
+import { UpdateUserBulkInsertInput } from "./dto/update-user-bulk-insert.dto";
+//import { UpdateUserInput } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -39,10 +40,20 @@ export class UsersResolver {
   @Mutation(() => User)
   updateUser(
     @Context() context: IContext,
-    @Args("updateUserInput") updateUserInput: UpdateUserInput,
+    @Args("updateUserBulkInsertInput")
+    updateUserBulkInsertInput: UpdateUserBulkInsertInput,
   ): Promise<User> {
     const id = context.req.user.id;
-    return this.usersService.update({ id, updateUserInput });
+    return this.usersService.update({ id, updateUserBulkInsertInput });
+  }
+
+  @Mutation(() => User)
+  updateUserPwd(
+    @Context() context: IContext,
+    @Args("password") password: string,
+  ): Promise<User> {
+    const id = context.req.user.id;
+    return this.usersService.updatePassword({ id, password });
   }
 
   @Mutation(() => Boolean)
