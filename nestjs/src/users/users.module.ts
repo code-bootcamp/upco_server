2dac34847933b277953df2ff2eb7015abe31767f
turnaroundwoo <seowooo3117@gmail.com>
2023-03-28 15:25:48 +0900

    Feat: user API 수정 (interest 부분 제거)

--- nestjs/src/users/users.module.ts
+++ nestjs/src/users/users.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Interest } from "src/interests/entities/interest.entity";
-import { InterestsService } from "src/interests/interests.service";
 import { User } from "./entities/user.entity";
 import { UsersResolver } from "./users.resolver";
 import { UsersService } from "./users.service";
@@ -10,14 +8,12 @@ import { UsersService } from "./users.service";
   imports: [
     TypeOrmModule.forFeature([
       User, //
-      Interest,
     ]),
   ],
 
   providers: [
     UsersResolver, //
     UsersService,
-    InterestsService,
   ],
 
   exports: [
