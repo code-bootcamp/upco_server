1f185321af3cea2910645c464801bf0ae33d49d8
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-03-28 11:43:34 +0900

    Feat: 토큰 주석 처리

--- nestjs/src/auth/auth.service.ts
+++ nestjs/src/auth/auth.service.ts
@@ -54,12 +54,12 @@ export class AuthService {
       { secret: process.env.JWT_REFRESH_KEY, expiresIn: "2w" },
     );
 
-    res.setHeader(
-      "Set-Cookie",
-      `refreshToken=${refreshToken}; path=/; domain=.api.upco.space; SameSite=None; Secure; httpOnly`,
-    );
-    res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader(
+    //   "Set-Cookie",
+    //   `refreshToken=${refreshToken}; path=/; domain=.api.upco.space; SameSite=None; Secure; httpOnly`,
+    // );
+    // res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
   getAccessToken({ user }: IAuthServiceGetAccessToken): string {
