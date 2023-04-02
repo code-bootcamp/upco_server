e6ca10c92f27346738cf8897116f1cc8768b8286
김현준 <supply900219@gmail.com>
2023-04-02 17:57:27 +0900

    Feat: 체팅방 리스트, 공지사항 수정
    index.js - chatRoomList 불러오기 로직 추가
    
    fetchNotice 추가

--- nestjs/src/notices/notices.service.ts
+++ nestjs/src/notices/notices.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotAcceptableException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Notice } from "./entites/notice.entity";
-import { INoticesServiceCreate } from "./interfaces/notices-service.interface";
+import {
+  INoticesServiceCreate,
+  INoticesServiceFindOne,
+} from "./interfaces/notices-service.interface";
 
 @Injectable()
 export class NoticesService {
@@ -11,15 +14,30 @@ export class NoticesService {
     private readonly noticesRepository: Repository<Notice>,
   ) {}
 
-  findAll(): Promise<Notice[]> {
-    return this.noticesRepository.find();
+  noticeFindOne({ id }: INoticesServiceFindOne): Promise<Notice> {
+    const result = this.noticesRepository.findOne({ where: { id } });
+    if (!result) throw new NotAcceptableException();
+    return result;
   }
 
-  create({ createNoticeInput }: INoticesServiceCreate): Promise<Notice> {
+  async noticeFindAll(): Promise<Notice[]> {
+    const result = await this.noticesRepository.find();
+    return result.sort(
+      (a, b) =>
+        this.extractNumberFromDate(b.createAt) -
+        this.extractNumberFromDate(a.createAt),
+    );
+  }
+
+  createNotice({ createNoticeInput }: INoticesServiceCreate): Promise<Notice> {
     const { title, contents } = createNoticeInput;
     return this.noticesRepository.save({
       title,
       contents,
     });
+  }
+
+  extractNumberFromDate(date: Date): number {
+    return Number(date.toISOString().replace(/\D/g, ""));
   }
 }
